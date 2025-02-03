0e772f9e94c1a70df294d1cb2ad316cb0fcdbc0d
Mickilenzy <ilerioluwamichael297@gmail.com>
2025-02-03 11:49:23 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+num1 = 500
+num2 = 150
+
+print("Number 1", num1)
+print("Number 2", num2)
+print("Adiition :" , num1+num2)
+print("Difference :" , num1-num2)
+print("Product:" , num1*num2)
+print("Division :" , num1/num2)
+print("Floor Division :" , num1//num2)
+print("Modulus Operation :" , num1%num2)
+print("Square  :" , num2**num2)
+print("Square Root:" , num1**0.5)
+
+
+print("Equal ?" , num1==num2)
+print("Number1 greater?:" , num1>num2)
+print("Number 2 greater? :" , num1<num2)
+print("Not Equal ?" , num1!=num2)
+
+result = num1/2+num2*7+10
+print("Result of given eqaution is:", result)
